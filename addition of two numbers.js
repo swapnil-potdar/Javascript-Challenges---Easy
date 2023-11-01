377f8d2b5cbd4304d48531597ee2fbec3a6350d3
Swapnil Potdar <potdar.swapnil10@gmail.com>
2023-11-01 18:24:55 +0530

    The Light Bulb program

---
+++ addition of two numbers.js
@@ -0,0 +1,9 @@
+  var x = prompt("Enter a Value", "0");
+  var y = prompt("Enter a Value", "0");
+  var num1 = parseInt(x);
+  var num2 = parseInt(y);
+
+  
+const c=num1+num2;
+
+console.log("addition is "+c);
---
+++ array.js
@@ -0,0 +1,4 @@
+var a = ["a", "b", "c"];
+a.forEach(function(entry) {
+  console.log(entry);
+});
