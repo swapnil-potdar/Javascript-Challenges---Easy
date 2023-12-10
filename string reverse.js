c4a858fae011b20cbc4f85fcae1f7f638126065d
Swapnil Potdar <potdar.swapnil10@gmail.com>
2023-12-10 22:14:57 +0530

    string reverse program

--- string reverse.js
+++ string reverse.js
@@ -3,6 +3,7 @@ function string_reverse(str)
     return str.split("").reverse().join("");
 }
 
+
 console.log(string_reverse("w3resource"));
 console.log(string_reverse("www"));
 console.log(string_reverse("JavaScript"));
