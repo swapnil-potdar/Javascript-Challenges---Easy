32ede8d9da6d7430b30d62a29b61a95999b00b03
Swapnil Potdar <potdar.swapnil10@gmail.com>
2023-12-11 22:40:28 +0530

    find the longest string

---
+++ longest string.js
@@ -0,0 +1,9 @@
+function longest_string(str_ara) {
+  var max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
+}
+
+console.log(longest_string(['a', 'aa', 'aaa',
+                                          'aaaaa','aaaa']))
